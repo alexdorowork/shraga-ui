a0cc23bb5fad06a4ae49d53f31ddd8ac80295edb
lizozom <lizka.k@gmail.com>
2025-03-17 15:46:38 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
 import "react-toastify/dist/ReactToastify.css";
 
 import { Chat } from "./components/index.ts";
-import { createRoot } from "./CreateRoot.tsx";
 
 import "./globals.css";
 
 export type { ChatProps } from "./components/Chat";
-export { Chat, createRoot };
+export { createRoot } from "./CreateRoot";
+export { Chat };
 
